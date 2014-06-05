49567c58bfe19dec7b8422a67298025d83b13204
Josh Bavari <josh@raisemore.com>
2014-06-05 00:14:20 -0500

    Added angular module to be used for FacebookConnect using promises

---
+++ www/angular/facebookConnect.js
@@ -0,0 +1,37 @@
+angular.module('ngCordova.plugins.facebookConnect', [])
+
+.factory('$cordovaFacebook', ['$q', function($q) {
+
+  return {
+    init: function init(appId) {
+      var q = $q.defer();
+
+      try {
+        FacebookConnect.init(appId, function success(data) {
+          q.resolve(data);
+        }, function failed(error) {
+          q.reject(error);
+        });
+      } catch (ex) {
+        q.reject(ex);
+      }
+
+      return q.promise;
+    },
+    login: function login(params) {
+      var q = $q.defer();
+
+      try {
+        FacebookConnect.login(params, function success(response) {
+          q.resolve(response);
+        }, function failed(error) {
+          q.reject(error);
+        });
+      } catch (ex) {
+        q.reject(ex);
+      }
+
+      return q.promise;
+    } 
+  };
+}]);
